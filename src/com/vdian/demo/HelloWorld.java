2ba056c3b150b232a733125df6c6fa2ef98dd1cc
wubin <wubin@weidian.com>
2015-12-30 11:34:53 +0800

    try push

---
+++ src/com/vdian/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.vdian.demo;
+
+/**
+ * Created by wubin on 12/30/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
+}
